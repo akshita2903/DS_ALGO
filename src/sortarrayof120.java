7b95d78419139072316ed4148bfad51f4f909e2b
Akshita-web <you@akshita29320@gmail.com>
2021-04-25 12:03:38 +0530

    dutch national falg algo

---
+++ src/sortarrayof120.java
@@ -0,0 +1,33 @@
+public class sortarrayof120 {
+    void sort012(int a[], int n)
+{
+  int low=0,mid=0,high=n-1,temp=0;
+  while(mid<=high)
+  {
+      switch(a[mid])
+      {
+          case 0:
+          {
+              temp=a[mid];
+              a[mid]=a[low];
+              a[low]=temp;
+              mid++;
+              low++;
+              break;
+          }
+          case 1:{
+              mid++;
+              break;
+          }
+          case 2:{
+              temp=a[mid];
+              a[mid]=a[high];
+              a[high]=temp;
+              high--;
+              break;
+          }
+      }
+  }
+}
+    
+}
