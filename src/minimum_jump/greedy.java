11996e072b1f13a2eb95e411fa33f68826f1f530
Akshita-web <you@akshita29320@gmail.com>
2021-04-26 19:18:36 +0530

    added greedy solution

---
+++ src/minimum_jump/greedy.java
@@ -0,0 +1,32 @@
+package minimum_jump;
+import java.util.*;
+
+public class greedy {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)a[i]=sc.nextInt();
+      System.out.println(minjump(a));
+      sc.close();
+    }
+    static int minjump(int arr[])
+    {
+      
+        int n=arr.length;
+        int farthest=arr[0],curr=arr[0],jump=1;
+        if(arr[0]==0)return -1;
+        if(n==1) return 0;
+        for(int i=1;i<n;i++)
+        {
+            farthest=Math.max(i+arr[i],farthest);
+            if(i==curr) {
+                jump++;
+                curr=farthest;
+            }
+            if(curr>=n-1) break;
+        }
+        return curr>=n-1?jump:-1;
+
+    }
+}
