9fbf467b5fb5f31becd12805bac79e271ddbbf61
Akshita-web <you@akshita29320@gmail.com>
2021-04-27 14:16:42 +0530

    added comment

--- src/minimum_jump/greedy.java
+++ src/minimum_jump/greedy.java
@@ -15,11 +15,11 @@ public class greedy {
       
         int n=arr.length;
         int farthest=arr[0],curr=arr[0],jump=1;
-        if(arr[0]==0)return -1;
-        if(n==1) return 0;
+        if(arr[0]==0)return -1; // if a[0]===0->maximum jump we can take from a[0] is of 0 length,so unable to reach end
+        if(n==1) return 0; //if we are already at end of arry so jump require=0
         for(int i=1;i<n;i++)
         {
-            farthest=Math.max(i+arr[i],farthest);
+            farthest=Math.max(i+arr[i],farthest); //farthest ditance it can reach from i
             if(i==curr) {
                 jump++;
                 curr=farthest;
