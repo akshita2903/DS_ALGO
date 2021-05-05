8a9183e41c40395908f36b47877c77d3373d0051
Akshita-web <you@akshita29320@gmail.com>
2021-05-05 11:38:32 +0530

    isusbest

---
+++ src/issubset.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class issubset{
+public String isSubset( long a1[], long a2[], long n, long m) {
+    int n1=a1.length,n2=a2.length,c=0;
+    ArrayList<Long> l=new ArrayList<>();
+    for(int i=0;i<n2;i++) l.add(a2[i]);
+    for(int i=0;i<n1;i++)
+    {
+        if(l.contains(a1[i])) c++;
+    }
+    return c==n2 ?"Yes":"No";
+    
+    
+}
+}
