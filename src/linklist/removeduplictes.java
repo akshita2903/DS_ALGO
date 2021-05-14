9744b25db0ee74ad8613ce0629055fd0d0910b32
akshita2903 <68686051+akshita2903@users.noreply.github.com>
2021-05-14 16:16:49 +0530

    iterative appproach is better

---
+++ src/linklist/removeduplictes.java
@@ -0,0 +1,17 @@
+public class removeduplictes {
+  //iterative
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head==null || head.next==null) return head;
+         ListNode temp=head;
+         while(temp!=null && temp.next!=null)
+         {
+            if(temp.val==temp.next.val)
+                temp.next=temp.next.next;
+            else temp=temp.next;
+         }
+         return head;
+         
+         
+     } //time-> O(n) and space->(n)
+    
+}
