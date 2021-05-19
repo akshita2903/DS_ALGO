444e1ef18a4d2fd40dc4bae3fb42ec46181cd097
akshita2903 <68686051+akshita2903@users.noreply.github.com>
2021-05-19 14:39:29 +0530

    added intersection of two link list

---
+++ src/linklist/intersection_of_2_ll.java
@@ -0,0 +1,17 @@
+﻿public class intersection_of_2_ll {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        //if(headB==null || headA==null) return null;
+       ListNode x=headA,y=headB;
+        while(x!=y)
+        {
+            if(x==null) x=headB;
+            else x=x.next;
+            if(y==null) y=headA;
+            else y=y.next;
+        }
+
+        return x;
+
+    }
+}
