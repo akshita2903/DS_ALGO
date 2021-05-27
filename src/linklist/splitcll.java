065c1230224a199312130ea70be8ec2314c102e3
akshita2903 <68686051+akshita2903@users.noreply.github.com>
2021-05-27 10:59:09 +0530

    time compllexity is O(n) and space O(1)

---
+++ src/linklist/splitcll.java
@@ -0,0 +1,39 @@
+﻿public class splitcll {
+    void splitList(circular_LinkedList list)
+    {
+        Node head=list.head,slow=head,fast=head,fastpr=null,prev=null;
+        if(head==null|| head.next==null)
+        {
+            list.head1=head;
+            list.head2=null;
+        }
+        while( fast.next!=head)
+        {
+            prev=slow;
+            slow=slow.next;
+            fastpr=fast;
+            fast=fast.next.next;
+            if(fast==head) break;
+        }
+        if(fast==head)//even
+        {
+            prev.next=head;
+            fastpr.next.next=slow;
+            list.head1=head;
+            list.head2=slow;
+        }
+        else{
+            fast.next=slow.next;
+            list.head2=slow.next;
+            slow.next=head;
+            list.head1=head;
+
+
+        }
+
+
+
+
+    }
+          
+}
