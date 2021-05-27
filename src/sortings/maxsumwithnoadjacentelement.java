c70e910fbaf0cc419746864f4f57117e87949bb9
akshita2903 <68686051+akshita2903@users.noreply.github.com>
2021-05-27 13:04:48 +0530

    added important dp question

---
+++ src/sortings/maxsumwithnoadjacentelement.java
@@ -0,0 +1,14 @@
+﻿public class maxsumwithnoadjacentelement {
+    public int FindMaxSum(int arr[], int n)
+    {
+       int dp[]=new int[n+1];
+       dp[0]=0;dp[1]=arr[0];
+       for(int i=1;i<n;i++)
+       dp[i+1]=Math.max(dp[i],arr[i]+dp[i-1]);
+       return dp[n];
+
+    }
+
+}
+//time->O(n) for traversing
+//space->O(n) for dp array
