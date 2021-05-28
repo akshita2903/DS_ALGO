deaf0711656b3e2ee829dd5b9371886d63575087
akshita2903 <68686051+akshita2903@users.noreply.github.com>
2021-05-28 14:28:45 +0530

    added reverse doubly ll.java file

---
+++ src/linklist/reversedoublyll.java
@@ -0,0 +1,18 @@
+﻿package linklist;
+
+public class reversedoublyll {
+    public static Node reverseDLL(Node  head)
+{
+    //Your code here
+    Node newprev=null,newnext=null;
+    while(head!=null)
+    {
+       newnext=head.next;
+       head.next=newprev;
+       head.prev=newnext;
+       newprev=head;
+       head=newnext;
+    }
+    return newprev;
+} //time->O(n) and space->O(n)
+}
