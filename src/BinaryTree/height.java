5d6d6e75bda97a4c2246c71c5f1025a8c9791fcd
akshita2903 <68686051+akshita2903@users.noreply.github.com>
2021-05-28 14:49:09 +0530

    started

---
+++ src/BinaryTree/height.java
@@ -0,0 +1,27 @@
+﻿package BinaryTree;
+
+public class height {
+    height left,right;
+    height(height l,height r){
+this.left=l;
+this.right=r;
+    }
+    height start;
+     Solution s=new Solution();
+     int z=s.maxDepth(start);
+
+}
+
+
+
+   class Solution {
+
+       public int maxDepth(height root) {
+           if(root==null) return 0;
+
+          int     l=   maxDepth(root.left) ;
+                 int  r=maxDepth(root.right);
+              return Math.max(l+1,r+1);
+
+       }
+    }
