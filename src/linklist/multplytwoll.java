6948e9fd34091f4ccf042cb461537ae02dcaf211
akshita2903 <68686051+akshita2903@users.noreply.github.com>
2021-06-02 14:59:21 +0530

    added multipy o two ll

---
+++ src/linklist/multplytwoll.java
@@ -0,0 +1,24 @@
+﻿package linklist;
+
+public class multplytwoll {
+multplytwoll next;
+int data;
+
+public long multiplyTwoLists(multplytwoll l1,multplytwoll l2){
+    Long ans=0L,ans2=0L,mod=1000000007L;
+    while(l1!=null)
+    {
+        ans=((ans*10)%mod+l1.data);
+        l1=l1.next;
+    }
+     while(l2!=null)
+    {
+        ans2=((ans2*10)%mod+l2.data);
+        l2=l2.next;
+    }
+
+    return ((ans%mod)*(ans2%mod))%mod;
+
+
+}
+    } //time->O(n) space->O(1)
