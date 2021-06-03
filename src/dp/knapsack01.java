1c1e9c81cf7d432dcd1719f42d1e9b4d59e9094f
akshita2903 <68686051+akshita2903@users.noreply.github.com>
2021-06-03 18:27:22 +0530

    0-1 knapsack

---
+++ src/dp/knapsack01.java
@@ -0,0 +1,19 @@
+﻿package dp;
+
+public class knapsack01{
+    static int knapSack(int W, int wt[], int val[], int n)
+    {
+        int dp[][]=new int[W+1][n+1];
+        for(int i=1;i<=W;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(wt[j-1]<=i)
+    dp[i][j]=Math.max(dp[i-wt[j-1]][j-1]+val[j-1],dp[i][j-1]);
+    else dp[i][j]=dp[i][j-1];
+            }
+        }
+        return dp[W][n];
+    }
+}
+//time->O(W*n) and space->O(W*n)
