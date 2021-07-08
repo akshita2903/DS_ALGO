4ef27be65cfd47c25d1c7deb1456f5a1ade26602
akshita2903 <akshita29320@gmail.com>
2021-07-08 11:29:17 +0530

    gfg

---
+++ src/sortings/triplets_with_sum_lessthan_x.java
@@ -0,0 +1,32 @@
+package sortings;
+import java.util.*;
+public class triplets_with_sum_lessthan_x {
+    long countTriplets(long arr[], int n,int sum)
+    {
+Arrays.sort(arr);
+return counted(arr,n,sum);
+    }
+    long counted(long a[],int n,int x){
+        long c=0L,sum=0L;
+        for(int i=0;i<n-2;i++){
+            int left=i+1,right=n-1;
+            while(left<n && left<right){
+                sum=a[i]+a[left]+a[right];
+                if(sum>=x) right--;
+                else{
+                   c+=(right-left); //before a[right] sum is always x
+                   left++;
+                }
+            }
+        }
+          
+        
+        return c;
+    }
+    
+}
+//two-pointer + sorting
+//time->O(n^2) and space->O(1)-------------->BETTER
+//----naive------
+//time->O(n^3)  using three for loops
+
