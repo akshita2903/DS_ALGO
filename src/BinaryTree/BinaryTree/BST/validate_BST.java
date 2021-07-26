f42d0f5436a66bc176b035fa4fb26772e16e7b2d
akshita2903 <akshita29320@gmail.com>
2021-07-26 12:03:37 +0530

    leetcode bst

---
+++ src/BinaryTree/BinaryTree/BST/validate_BST.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class TreeNode {
+    TreeNode left,right; int val;
+}
+class validate_BST {
+    public boolean isValidBST(TreeNode root) {
+       return isvalid(root);
+    }
+    boolean isvalid(TreeNode root){
+        ArrayList<Integer> l=new ArrayList<>();
+    inorder(root,l);
+       // int pre=l.get(0);
+        for(int i=1;i<l.size();i++){
+            int x=l.get(i);
+            int pre=l.get(i-1);
+            if(x<=pre) return false;
+            
+        }
+        return true;
+    }
+    void inorder(TreeNode r,ArrayList<Integer> l){
+        if(r==null) return;
+        inorder(r.left,l);
+        l.add(r.val);
+        inorder(r.right,l);
+    }
+       
+}
