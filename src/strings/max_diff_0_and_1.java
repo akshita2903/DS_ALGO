83242777329ac2b03a1f70fca912bee91bb5c4ea
akshita2903 <akshita29320@gmail.com>
2021-08-16 19:08:57 +0530

    kandanes's Algo

---
+++ src/strings/max_diff_0_and_1.java
@@ -0,0 +1,23 @@
+package strings;
+class max_diff_0_and_1{
+    public static void main(String[] args){
+         int maxSubstring(String S) {
+        // code here
+          int g_max=-1,l_max=0;
+          for(int i=0;i<S.length();i++)
+          {
+             char y=getchar(S,i);
+            
+         l_max+=(int)y==48?1:-1;
+             if(l_max>g_max) g_max=l_max;
+             if(l_max<0) l_max=0;
+          }
+         
+          return g_max;
+
+    }
+    char getchar(String s,int i){
+        return s.charAt(i);
+    }
+    }
+}
