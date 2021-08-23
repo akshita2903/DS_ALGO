95e24c255bccb7de21a32f750d25bdcf358d05fe
akshita2903 <akshita29320@gmail.com>
2021-08-23 15:02:42 +0530

    LPSubsequence using LPSubstring and LCsubsequnece

--- src/dp/longest_palindrome_subsequence.java
+++ src/dp/longest_palindrome_subsequence.java
@@ -1,5 +1,26 @@
 package dp;
 class longest_palindrome_subsequence{
+    //using LPS--------------------
+      public int longestPalindromeSubseq(String s)
+    {
+        int n=s.length();
+        int[][] dp=new int[n][n];
+        //for string of length 1
+        for(int i=0;i<n;i++) dp[i][i]=1;
+        
+        //Max possible len f palindrome subsequnec
+        for(int len_of_str=2;len_of_str<=n;len_of_str++){
+            //run from 0 to n-(char after i+len)
+            for(int i=0;i<=n-len_of_str;i++){
+                int j=i+len_of_str-1;//get last char of substring of length //len starting from ith index
+                if(getc(s,i)==getc(s,j))
+                    dp[i][j]=2+dp[i+1][j-1];
+                else dp[i][j]=Math.max(dp[i+1][j],dp[i][j-1]);
+            }
+        }
+        return dp[0][n-1];
+    }
+    //using LCS||-------------------
   static int LPS(String S) {
         int n=S.length();
         String y=reverse(S);
