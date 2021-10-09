d6f5bd6cbcb22a6be22301c0259cda0f04838a9a
akshita2903 <68686051+akshita2903@users.noreply.github.com>
2021-10-09 19:15:20 +0530

    Update UnionSetAndPathCompression.java

--- src/graph/UnionSetAndPathCompression.java
+++ src/graph/UnionSetAndPathCompression.java
@@ -1,1 +1,21 @@
-
+package graph;
+import java.util.*;
+class UnionSetAndPathCompression{
+  int[] rank=new int[100+1];
+    
+	int find(int A[],int X)
+        {
+          if(A[X]==X)
+          return X;
+          return A[X]=find(A,A[X]);
+	}
+	void unionSet(int A[],int X,int Z)
+        {
+            
+         int parx=find(A,X);
+         int parz=find(A,Z);
+    //   if(X==Z) return ;
+       A[parx]=parz;
+         
+	}
+}
